494070cdaa703d0d5da000091b62e8e47c6ba4b1
vann <vann@innerfusion.net>
2009-06-03 00:09:27 -0500

    added condition to only extend Test::Unit if Rails < 2.3

--- init.rb
+++ init.rb
@@ -3,5 +3,5 @@ require 'ar_helper'
 ActiveRecord::Base.send :include, ArHelper
 ActionView::Base.send :include, ArHelper::Sugar
 ActionController::Base.send :include, ArHelper::Sugar
-# so we can have some sugar with our tests too
-Test::Unit::TestCase.send :include, ArHelper::Sugar
+# so we can have some sugar with our tests too only if you are using Rails < 2.3.2
+Test::Unit::TestCase.send :include, ArHelper::Sugar if RAILS_GEM_VERSION < '2.3.2'

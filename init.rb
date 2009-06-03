96547287f7950e7dd8ca71f4851e8d59d5ff0717
vann <vann@innerfusion.net>
2009-06-03 00:28:01 -0500

    added includes so that you can use sugar methods in rails > 2.3 testing env

--- init.rb
+++ init.rb
@@ -5,3 +5,6 @@ ActionView::Base.send :include, ArHelper::Sugar
 ActionController::Base.send :include, ArHelper::Sugar
 # so we can have some sugar with our tests too only if you are using Rails < 2.3.2
 Test::Unit::TestCase.send :include, ArHelper::Sugar if RAILS_GEM_VERSION < '2.3.2'
+# make sure that we can make it work with Rails >= 2.3.2
+ActionController::TestCase.send :include, ArHelper::Sugar if RAILS_GEM_VERSION >= '2.3.2'
+ActiveSupport::TestCase.send :include, ArHelper::Sugar if RAILS_GEM_VERSION >= '2.3.2'
